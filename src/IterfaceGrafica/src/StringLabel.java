4414851f6bdb07ab068e69c797f8935f7a07a594
Beatriz <beatriz.vsiqueira@gmail.com>
2019-05-13 23:41:47 -0300

    Mudulo Grafico da Bia- Tirando umas coisas nada a ver

---
+++ src/IterfaceGrafica/src/StringLabel.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class StringLabel {
+    String fala;
+
+    public StringLabel(String fala){
+        this.fala = fala;
+    }
+
+    JLabel getStringLabel(){
+        JLabel label = new JLabel(this.fala);
+        return label;
+    }
+
+}
